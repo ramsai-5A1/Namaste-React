b13f89baea63fbe5676eb84de0339c2453452088
Ramsai <ramsaicrony@gmail.com>
2024-01-07 17:07:30 +0530

    Used fireEvent to simulate onClick login button

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import "@testing-library/jest-dom"
 import { Provider } from "react-redux";
@@ -22,5 +22,32 @@ describe("Header-Component testing", () => {
 
         const cartItems = screen.getByText("Cart (0 items)");
         expect(cartItems).toBeInTheDocument();
+
+        //Regex
+        const onlineStatus = screen.getByText(/Online/);
+        expect(onlineStatus).toBeInTheDocument();
     });
+
+    test("Should change from Login button to Logout button onClick", () => {
+        render(
+            <BrowserRouter>
+            <Provider store={AppStore}>
+                <Header/>
+            </Provider>
+            </BrowserRouter>
+        );
+        
+        const loginButton = screen.getByRole("button", { name: "Login" });
+        expect(loginButton).toBeInTheDocument();
+
+        fireEvent.click(loginButton);
+
+        const logoutButton = screen.getByRole("button", { name: "Logout" });
+        expect(logoutButton).toBeInTheDocument();
+
+        fireEvent.click(logoutButton);
+        
+        const loginButtonAgain = screen.getByRole("button", { name: "Login" });
+        expect(loginButtonAgain).toBeInTheDocument();
+    })
 });
