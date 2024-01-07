27233bc23ab89c6d3399dddc0408af549b0b1f9f
Ramsai <ramsaicrony@gmail.com>
2024-01-07 17:40:27 +0530

    Added test-cases for promotedRestraCard component and normalRestraCard component

--- src/components/__tests__/Restracard.test.js
+++ src/components/__tests__/Restracard.test.js
@@ -1,8 +1,10 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import Restracard from "../Restracard";
 import MOCK_DATA from "../mocks/RestracardMock.json"
+import MOCK_DATA_PROMOTED_ONE from "../mocks/RestracardPromotedMock.json";
 import { BrowserRouter } from "react-router-dom";
 import "@testing-library/jest-dom"
+import { withPromotedLabel } from "../Restracard";
 
 describe("RestraCard component testing", () => {
     test("Should render restraCard component by passing props data", () => {
@@ -21,4 +23,19 @@ describe("RestraCard component testing", () => {
         fireEvent.click(exploreButton);
         expect(window.location.pathname).toBe("/resturants");
     });
+
+    test("Should render promotedRestraCard", () => {
+        const ResturantCardPromoted = withPromotedLabel(Restracard);
+        render(
+            <BrowserRouter>
+                <ResturantCardPromoted dataObj={MOCK_DATA_PROMOTED_ONE}/>
+            </BrowserRouter>
+        );
+
+        const promotedLabel = screen.getByText("Promoted");
+        expect(promotedLabel).toBeInTheDocument();
+
+        const title = screen.getByText("Chapati Curry");
+        expect(title).toBeInTheDocument();
+    })
 });
